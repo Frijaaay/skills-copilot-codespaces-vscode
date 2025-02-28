af1bbfa991a026a58306f1224e646d785be6d02e
Jay Cortez Jr. <119042660+Frijaaay@users.noreply.github.com>
2025-02-28 15:07:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {}
+function skillsLeader() {}
+function skillsAdmin() {}
+function skillsSuperAdmin() {}
+
+module.exports = {
+   skillsMember,
+   skillsLeader,
+   skillsAdmin,
+   skillsSuperAdmin
+}
